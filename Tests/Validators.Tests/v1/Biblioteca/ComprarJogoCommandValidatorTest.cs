99a450c5b50c0fa862dd70ee4eb7d9de93b5a003
antonioRenato <antoniorpfs@gmail.com>
2025-12-20 16:19:52 -0300

    feat: created tests

---
+++ Tests/Validators.Tests/v1/Biblioteca/ComprarJogoCommandValidatorTest.cs
@@ -0,0 +1,45 @@
+﻿using CommonTestUtilities.Commands.Biblioteca;
+using Domain.Commands.v1.Biblioteca.ComprarJogo;
+using FluentAssertions;
+
+namespace Validators.Tests.v1.Biblioteca
+{
+    public class ComprarJogoCommandValidatorTest
+    {
+        private readonly ComprarJogoCommandValidator _validator = new();
+
+        [Fact]
+        public void Deve_passar_quando_dados_estao_validos()
+        {
+            var request = ComprarJogoCommandBuilder.Build();
+
+            var result = _validator.Validate(request);
+
+            result.IsValid.Should().BeTrue();
+        }
+        [Fact]
+        public void Deve_falhar_quando_id_user_estiver_vazio()
+        {
+            var request = ComprarJogoCommandBuilder.BuildWithId(Guid.Empty, Guid.NewGuid());
+
+            var result = _validator.Validate(request);
+
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().Contain(e =>
+                e.PropertyName == "IdUsuario" &&
+                e.ErrorMessage == "O ID do usuário não pode ser um GUID vazio.");
+        }
+        [Fact]
+        public void Deve_falhar_quando_id_jogo_estiver_vazio()
+        {
+            var request = ComprarJogoCommandBuilder.BuildWithId(Guid.NewGuid(), Guid.Empty);
+
+            var result = _validator.Validate(request);
+
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().Contain(e =>
+                e.PropertyName == "IdJogo" &&
+                e.ErrorMessage == "O ID do jogo não pode ser um GUID vazio.");
+        }
+    }
+}
