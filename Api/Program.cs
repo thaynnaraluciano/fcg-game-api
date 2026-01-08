2b19ce5f327ee891005ec21e96166d0751201e87
Zezao1224 <gamer12224@yahoo.com.br>
2026-01-08 16:49:51 -0300

    Adicionando var de ambiente ao elastic Search

--- Api/Program.cs
+++ Api/Program.cs
@@ -111,6 +111,11 @@ builder.Services.AddScoped<IBibliotecaRepository, BibliotecaRepository>();
 
 builder.Services.Configure<AppSettings>(builder.Configuration);
 builder.Services.AddSingleton<IMetricsService, MetricsService>();
+//Le variáveis do .env para o elasticSearch
+string elasticUrl = Environment.GetEnvironmentVariable("ElasticSearch_urlApi") ?? builder.Configuration["ElasticSearch:urlApi"] ?? "";
+string elasticKey = Environment.GetEnvironmentVariable("ElasticSearch_Key") ?? builder.Configuration["ElasticSearch:Key"] ?? "";
+builder.Configuration["ElasticSearch:urlApi"] = elasticUrl;
+builder.Configuration["ElasticSearch:Key"] = elasticKey;
 
 #if DEBUG
 //Chama o gerenciador do docker ANTES da aplicação iniciar
